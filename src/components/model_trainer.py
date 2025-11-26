0582fd7f9ee2c18e6856fce6f16cf34c8b6cbf32
Vineet Naik <vnn28@bath.ac.uk>
2025-11-26 19:20:23 +0000

    Fix formatting in AdaBoost Regressor parameters

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -62,7 +62,7 @@ class ModelTrainer:
                 'n_estimators': [8, 16, 32, 64, 128, 256]
             },
     
-            "AdaBoost Regressor": {  # ← MAKE SURE THIS EXISTS!
+            "AdaBoost Regressor": { 
                 'learning_rate': [.1, .01, 0.5, .001],
                 'n_estimators': [8, 16, 32, 64, 128, 256]
             }
